df2473529168dcb0aac1b072972f2ae3ef40efee
Praanesh Selvaraj <praaneshselvaraj2003@gmail.com>
2026-01-21 11:19:49 +0530

    symbols is all I need

--- lua/vimboi/plugins/nvim-cmp.lua
+++ lua/vimboi/plugins/nvim-cmp.lua
@@ -12,7 +12,7 @@ return { -- Autocompletion
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
-        return 'make install_jsregexp'  -- Because we all *love* making things harder with JS regex, right?
+        return 'make install_jsregexp' -- Because we all *love* making things harder with JS regex, right?
       end)(),
       dependencies = {
         -- `friendly-snippets` contains a variety of premade snippets.
@@ -31,18 +31,48 @@ return { -- Autocompletion
   config = function()
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    luasnip.config.setup {}  -- Because setting up snippets is *so* much fun.
+    luasnip.config.setup {} -- Because setting up snippets is *so* much fun.
+
+    -- Kind icons because plain text completion is for peasants
+    local kind_icons = {
+      Text = '󰦨',
+      Method = '󰆧',
+      Function = '󰊕',
+      Constructor = '',
+      Field = '',
+      Variable = '󰀫',
+      Class = '󰠱',
+      Interface = '',
+      Module = '',
+      Property = '󰜢',
+      Unit = '󰑭',
+      Value = '󰎠',
+      Enum = '',
+      Keyword = '󰌋',
+      Snippet = '',
+      Color = '󰏘',
+      File = '󰈙',
+      Reference = '',
+      Folder = '󰉋',
+      EnumMember = '',
+      Constant = '󰏿',
+      Struct = '',
+      Event = '',
+      Operator = '󰆕',
+      TypeParameter = '',
+      Misc = '',
+    }
 
     cmp.setup {
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)  -- Oh look, we're expanding snippets like it's an Olympic event.
+          luasnip.lsp_expand(args.body) -- Oh look, we're expanding snippets like it's an Olympic event.
         end,
       },
       completion = { completeopt = 'menu,menuone,noinsert' },
 
       -- For an understanding of why these mappings were
-      -- Don't worry, we know you won't. Just *trust* us on this one. 
+      -- Don't worry, we know you won't. Just *trust* us on this one.
       mapping = cmp.mapping.preset.insert {
         -- Select the [n]ext item, because the current one just isn’t as perfect as the next one.
         ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -73,31 +103,40 @@ return { -- Autocompletion
         -- Or think of it as the most pointless thing you’ll ever do today.
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()  -- Just jumping to the right, like you’re making life decisions at 3 AM.
+            luasnip.expand_or_jump() -- Just jumping to the right, like you’re making life decisions at 3 AM.
           end
         end, { 'i', 's' }),
         ['<C-h>'] = cmp.mapping(function()
           if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)  -- Because you *always* need to go backwards, right?
+            luasnip.jump(-1) -- Because you *always* need to go backwards, right?
           end
         end, { 'i', 's' }),
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        -- But let’s be real: you’re probably never going to read this link. #YouDoYou
+        -- But let's be real: you're probably never going to read this link. #YouDoYou
+      },
+      formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+          -- Show fancy icons next to completion items because we're all about that aesthetic life
+          local kind = vim_item.kind
+          vim_item.kind = string.format('%s', kind_icons[kind])
+          vim_item.menu = string.format('%s', kind)
+          return vim_item
+        end,
       },
       sources = {
         {
-          name = 'lazydev',  -- Yeah, we’re being lazy with this development process... and it’s working perfectly.
+          name = 'lazydev', -- Yeah, we’re being lazy with this development process... and it’s working perfectly.
           -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
           group_index = 0,
         },
-        { name = 'nvim_lsp' },  -- Because you *have* to have LSP completions, who cares about anything else?
-        { name = 'luasnip' },  -- Snippets are a *luxury*, and you're living the dream with them.
-        { name = 'path' },  -- Of course, because navigating paths manually was just way too easy.
-        { name = 'nvim_lsp_signature_help' },  -- Signatures... because looking at code is never enough without signatures, right?
+        { name = 'nvim_lsp' }, -- Because you *have* to have LSP completions, who cares about anything else?
+        { name = 'luasnip' }, -- Snippets are a *luxury*, and you're living the dream with them.
+        { name = 'path' }, -- Of course, because navigating paths manually was just way too easy.
+        { name = 'nvim_lsp_signature_help' }, -- Signatures... because looking at code is never enough without signatures, right?
       },
     }
   end,
 }
-
